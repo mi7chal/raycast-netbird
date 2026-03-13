181f3325b0d590edd077305ad67c2da3a64d5650
Michal Chruscielski <michalch775@gmail.com>
2026-03-13 16:15:11 +0100

    feat: release ready

--- src/update.ts
+++ src/update.ts
@@ -1,4 +1,4 @@
-import { showToast, Toast, open } from "@raycast/api";
+import { showToast, Toast } from "@raycast/api";
 import { netbirdUpdate } from "./utils";
 
 export default async function main() {
